ce903b044c3c018f4791f2a7177f90178982203b
Isaacsagni <160358867+Isaacsagni@users.noreply.github.com>
2025-01-28 00:45:47 +0300

    Add files via upload
    Assigns a unique row number to each partition of duplicates based on specified columns.

---
+++ Adding a sequentil no to each row.sql
@@ -0,0 +1,28 @@
+SELECT * 
+FROM layoffs;
+CREATE TABLE `layoffs_staging` (
+  `company` text,
+  `location` text,
+  `industry` text,
+  `total_laid_off` int DEFAULT NULL,
+  `percentage_laid_off` text,
+  `date` text,
+  `stage` text,
+  `country` text,
+  `funds_raised_millions` int DEFAULT NULL,
+  `row_num` int
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+    SELECT *,
+           ROW_NUMBER() OVER (
+               PARTITION BY company, location, industry, percentage_laid_off, `date`, stage, country, funds_raised_millions
+           ) AS row_num
+    FROM layoffs
+
+
+
+
+
+
+
